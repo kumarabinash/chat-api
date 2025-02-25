d44caaaa27333546630b0219da77ca0ae621aa50
Kumar Abinash <abi.cd.sh@gmail.com>
2025-02-25 11:28:05 +0100

    User join and leave

--- handlers/chatHandlers.ts
+++ handlers/chatHandlers.ts
@@ -4,6 +4,7 @@ import { CHAT_EVENTS } from '../types/events.enum';
 export function setupChatHandlers(socket: Socket, io: Server) {
 
     socket.on(CHAT_EVENTS.CHAT_MESSAGE, (message: string) => {
+        console.log(message);
         io.emit(CHAT_EVENTS.CHAT_MESSAGE_RECEIVED, { userId: socket.id, message });
     });
 }
--- handlers/userHandlers.ts
+++ handlers/userHandlers.ts
@@ -1,11 +1,25 @@
 import { Server, Socket } from 'socket.io';
-import { USER_EVENTS } from '../types/events.enum';
+import { CHAT_EVENTS, USER_EVENTS } from '../types/events.enum';
 export function setupUserHandlers(socket: Socket, io: Server) {
     socket.on(USER_EVENTS.USER_JOIN_REQUEST, (username: string) => {
-        io.emit(USER_EVENTS.USER_JOINED, { userId: socket.id, username });
+        io.emit(CHAT_EVENTS.CHAT_MESSAGE_RECEIVED, {
+            userId: socket.id, message: {
+                sender: username,
+                message: `${username} joined the chat`,
+                timestamp: new Date().toISOString(),
+                type: 'system'
+            }
+        });
     });
 
-    socket.on(USER_EVENTS.USER_DISCONNECT, () => {
-        io.emit(USER_EVENTS.USER_LEFT, { userId: socket.id });
+    socket.on(USER_EVENTS.USER_DISCONNECT, (username: string) => {
+        io.emit(CHAT_EVENTS.CHAT_MESSAGE_RECEIVED, {
+            userId: socket.id, message: {
+                sender: username,
+                message: `${username} left the chat`,
+                timestamp: new Date().toISOString(),
+                type: 'system'
+            }
+        });
     });
 }
